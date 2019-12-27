dd114201e1b64026a4f7a0513ed763a6fbb0d775
Mikhail Borisov <mishaborisov2013@gmail.com>
2019-12-27 20:27:22 +0300

    Initial Commit

---
+++ Playground/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Playground
+//
+//  Created by Михаил Борисов on 27.12.2019.
+//  Copyright © 2019 Mikhail Borisov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
